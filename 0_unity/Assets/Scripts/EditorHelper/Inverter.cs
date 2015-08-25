91db2cfcb643736ebbc8941eef2b5118a1112cd3
Vincent Mahnke <accounts@vimaster.de>
2015-08-25 02:29:27 +0200

    + Added map-symmetry editor helper script

---
+++ 0_unity/Assets/Scripts/EditorHelper/Inverter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Inverter : MonoBehaviour {
+    [MenuItem("CONTEXT/Transform/Flip object")]
+    private static void SetOffset(MenuCommand menuCommand)
+    {
+        (menuCommand.context as Transform).localPosition = new Vector3(
+            (menuCommand.context as Transform).localPosition.x * -1,
+            (menuCommand.context as Transform).localPosition.y,
+            (menuCommand.context as Transform).localPosition.z
+        );
+        (menuCommand.context as Transform).localEulerAngles = new Vector3(
+            (menuCommand.context as Transform).localEulerAngles.x,
+            180 - (menuCommand.context as Transform).localEulerAngles.y,
+            (menuCommand.context as Transform).localEulerAngles.z
+        );
+    }
+}
