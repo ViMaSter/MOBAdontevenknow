5a1a53b28a64b526472089a9b0208e6897ab4561
Vincent Mahnke <accounts@vimaster.de>
2015-08-26 09:06:17 +0200

    + Added running animation

---
+++ 0_unity/Assets/Scripts/Interfaces/PlayerAnimationProxy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationProxy : MonoBehaviour
+{
+    public Animator Animator;
+    public NavMeshAgent NavMeshAgent;
+    public AttackBehaviour AttackBehaviour;
+
+	void Update () {
+        foreach (AnimatorControllerParameter parameter in Animator.parameters)
+        {
+            switch (parameter.name)
+            {
+                case "currentMovementSpeed":
+                    Animator.SetFloat(parameter.nameHash, NavMeshAgent.velocity.magnitude);
+                    break;
+                case "isAttacking":
+                    Animator.SetBool(parameter.nameHash, AttackBehaviour.CurrentEnemy != null);
+                    break;
+            }
+        }
+	}
+}
